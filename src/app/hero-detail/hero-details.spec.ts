b2fdaa459693ce4381bfe0d968a4c1fe621fac26
Marko Bojic <markobo@ccbill.com>
2018-10-09 12:49:18 +0200

    Fake async testing

--- src/app/hero-detail/hero-details.spec.ts
+++ src/app/hero-detail/hero-details.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestBed, ComponentFixture, fakeAsync, tick, flush } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -38,4 +38,14 @@ describe('Hero Details', () => {
 
     expect(fixture.nativeElement.querySelector('h2').textContent).toContain('MARKO');
   });
+
+  it('should call updateHero when save is called', fakeAsync(() => {
+    mockHeroService.updateHero.and.returnValue(of({}));
+    fixture.detectChanges();
+
+    fixture.componentInstance.save();
+    flush();
+
+    expect(mockHeroService.updateHero).toHaveBeenCalled();
+  }));
 });
