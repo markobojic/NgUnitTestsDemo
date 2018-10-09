d3d756141793a7baa3d1e85f24ec9f45202c131c
Marko Bojic <markobo@ccbill.com>
2018-10-09 11:28:00 +0200

    Testing DOM Interaciton - Hero Details with Activated route service

---
+++ src/app/hero-detail/hero-details.spec.ts
@@ -0,0 +1,41 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './../hero.service';
+
+describe('Hero Details', () => {
+  let fixture: ComponentFixture<HeroDetailComponent>;
+  let mockHeroService, mockActivatedRoute, mockLocation;
+
+  beforeEach(() => {
+    mockActivatedRoute = {
+      snapshot: { paramMap: { get: () => '3' }}
+    };
+    mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']);
+    mockLocation = jasmine.createSpyObj(['back']);
+
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [HeroDetailComponent],
+      providers: [
+        {provide: HeroService, useValue: mockHeroService},
+        {provide: ActivatedRoute, useValue: mockActivatedRoute},
+        {provide: Location, useValue: mockLocation}
+      ]
+    });
+
+    fixture = TestBed.createComponent(HeroDetailComponent);
+
+    mockHeroService.getHero.and.returnValue(of({id: 3, name: 'Marko', strength: 100}));
+  });
+
+  it('should render hero name in a h2 tag', () => {
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('h2').textContent).toContain('MARKO');
+  });
+});
